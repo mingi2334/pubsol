1ad14fd070213558b417608af030989dcfbbcf08
mingi2334 <mangogood1004@gmail.com>
2021-11-22 15:18:26 +0900

    a

--- 7/js/script.js
+++ 7/js/script.js
@@ -6,3 +6,13 @@ $(".gnb li").hover(
         $(this).children(".sub").stop().slideUp();  
     }
 );
+$(".open").click(
+    function(){
+  $("#popup").fadeIn();  
+    }
+);
+$(".close").click(
+    function(){
+  $("#popup").fadeOut();  
+    }
+);
